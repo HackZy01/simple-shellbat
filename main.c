186fddb19e7d16cd3b3ff5aaaab3fe9213d0e776
Dawid <77678316+HackZy01@users.noreply.github.com>
2025-06-08 15:50:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -128,12 +128,10 @@ static int status_draw_time_patched(void *a1, int a2)
         }
         scePafWidgetSetFontSize(a1, 20.0, 1, bat_num_start, bat_num_len);
         scePafWidgetSetFontSize(a1, 16.0, 1, percent_start, 1);
-	//plug in and unplug animations (shrinks and grows)
+	//Shrink when plugged in or out
         int is_charging = scePowerIsBatteryCharging(); 
         if (is_charging != was_charging) { 
-            scePafWidgetSetFontSize(a1, 17.0f, 1, bat_num_start, bat_num_len);
-	    scePafWidgetSetFontSize(a1, 17.0, 1, percent_start, 1);
-
+            scePafWidgetSetFontSize(a1, 18.0f, 1, bat_num_start, bat_num_len);
         }
 	was_charging = is_charging;
     }
